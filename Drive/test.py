6dce748337a4778c86b8dba8349b7b15ab063ad4
michelvaillant78 <72010572+michelvaillant78@users.noreply.github.com>
2024-04-26 14:03:53 +0200

    Update test

--- Drive/test.py
+++ Drive/test.py
@@ -63,7 +63,7 @@ def sonar_ping():
 
 # Création d'un socket pour écouter les commandes
 serveur = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-serveur.bind(('192.168.0.18', 12345))  # Remplacez l'adresse IP par celle de votre Raspberry Pi
+serveur.bind(('10.38.165.197', 12345))  # Remplacez l'adresse IP par celle de votre Raspberry Pi
 serveur.listen(5)
 
 print("En attente de connexions...")
